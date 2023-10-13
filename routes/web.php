1b9777105e96fc7143becc228bacd25c6cb066b8
3B032111 <3b032111@gm.student.ncut.edu.tw>
2023-10-13 11:37:53 +0800

    git practice 2-1 and 2-2 hello tom

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,6 @@ Route::get('r1', function() {  //git practice 1-3  redirect routes r1 to r2
 Route::get('r2', function() {
     return view('welcome');
 });
-Route::get('hello/{name?}', function($name) { //git practice 2-1  hello tom
+Route::get('hello/{name?}', function($name) { //git practice 2-1 and 2-2 hello tom
     return 'Hello, '.$name;
 });
